14de24ebc4d2ae66982dac0f541e0807ddc9669e
DiaaDiab <diaa.diab.2012@gmail.com>
2013-04-16 20:55:05 +0200

    Move easy for user interface

--- SQL_easy.py
+++ SQL_easy.py
@@ -12,33 +12,48 @@ import readline
 
 class SQL_easy(object):
 
-    def MySQL_Screen(self):
+    def MySQL_Screen_1(self):
         print
         print '-----------------------------------------'
         print '[01]Show DataBases On Server. '
         print '[02]Create New DataBase. '
-        print '[03]Show Tables In DataBase. '
-        print '[04]Create New Table. '
-        print '[05]Delete DataBase From Server. '
-        print '[06]Delete Table From DataBase. '
-        print '[07]Take MySQL Shell. '
-        print '[08]Show All Records In Table. '
-        print '[09]Add Record To Table. '
-        print '[10]Update Record In Table. '
-        print '[11]View Specific Record From Table. '
-        print '[12]Take Dump From DataBase. '
+        print '[03]Delete DataBase From Server. '
+        print '[04]Use One Of DataBase.'
+        print '[05]Take MySQL Shell. '
+        print '[06]Take Dump From DataBase. '
         print '[00]Quit. '
         print '-----------------------------------------'
         print '***********Choose By Number***********'
 
-    def repeat(self):
+    def MySQL_Screen_2(self):
+        print '-----------------------------------------'
+        print '[01]Show Tables In DataBase. '
+        print '[02]Create New Table. '
+        print '[03]Show All Records In Table. '
+        print '[04]Add Record To Table. '
+        print '[05]Update Record In Table. '
+        print '[06]View Specific Record From Table. '
+        print '[07]Delete Table From DataBase. '
+        print '[08]Exit Current DataBase. '
+        print '[00]Quit. '
+        print '-----------------------------------------'
+        print '***********Choose By Number***********'
+
+    def repeat_1(self):
         ch = raw_input('[**](Q)uite, else Continue: ')
         ch = ch.lower()
         if ch == 'q':
             sys.exit()
         else:
-            self.MySQL_Screen()
+            self.MySQL_Screen_1()
 
+    def repeat_2(self):
+        ch = raw_input('[**](Q)uite, else Continue: ')
+        ch = ch.lower()
+        if ch == 'q':
+            sys.exit()
+        else:
+            self.MySQL_Screen_2()
 
 def main():
     print '''
@@ -60,67 +75,73 @@ def main():
     if MyConnection != None and Cursor != None:
         # display screen for user
         showing = SQL_easy()
-        showing.MySQL_Screen()
+        showing.MySQL_Screen_1()
     while True:
         ch = raw_input('[**]Choice:')
+
         if ch == '1':
             struc.Show_dbs()
-            showing.repeat()
+            showing.repeat_1()
+
         if ch == '2':
             databaseName = raw_input('[*]Enter Your New DataBase Name: ')
             struc.Create_db(databaseName)
-            showing.repeat()
+            showing.repeat_1()
+
         if ch == '3':
-            databaseName = raw_input('[*]Enter DataBase That You Want To Show Tables In It: ')
-            struc.Show_tables(databaseName)
-            showing.repeat()
-        if ch == '4':
-            databaseName = raw_input('[*]Enter Which DataBase Name To Create Table In It: ')
-            deal.Change_db(databaseName)
-            tableName = raw_input('[*]Enter Your New Table Name: ')
-            struc.Create_table(tableName)
-            showing.repeat()
-        if ch == '5':
             databaseName = raw_input('[*]Enter DataBase Name That You Want To Drop It Down: ')
             struc.Drop_db(databaseName)
-            showing.repeat()
-        if ch == '6':
-            databaseName = raw_input('[*]Enter DataBase Name That You Want TO Drop Table from It: ')
+            showing.repeat_1()
+
+        if ch == '4':
+            databaseName = raw_input('[*]Enter DataBase Name That You Want To Use It: ')
             deal.Change_db(databaseName)
-            tableName = raw_input('[*]Enter Table Name That You Want To Drop It: ')
-            struc.Drop_table(tableName)
-            showing.repeat()
-        if ch == '7':
+            showing.MySQL_Screen_2()
+            while True:
+                ch = raw_input('[**] Choice: ')
+
+                if ch == '1':
+                    struc.Show_tables(databaseName)
+                    showing.repeat_2()
+
+                if ch == '2':
+                    tableName = raw_input('[*]Enter Your New Table Name: ')
+                    struc.Create_table(tableName)
+                    showing.repeat_2()
+                if ch == '3':
+                    tableName = raw_input('[*]Enter Table Name That You Want To Deal With It: ')
+                    deal.View_all(tableName, databaseName)
+                    showing.repeat_2()
+                if ch == '4':
+                    tableName = raw_input('[*]Enter Table Name That You Want To Deal With It: ')
+                    deal.Add(tableName)
+                    showing.repeat_2()
+                if ch == '5':
+                    tableName = raw_input('[*]Enter Table Name That You Want To Deal With It: ')
+                    deal.Update(tableName)
+                    showing.repeat_2()
+
+                if ch == '6':
+                    tableName = raw_input('[*]Enter Table Name That You Want To Deal With It: ')
+                    deal.View(tableName, databaseName)
+                    showing.repeat_2()
+                if ch == '7':
+                    tableName = raw_input('[*]Enter Table Name That You Want To Drop It: ')
+                    struc.Drop_table(tableName)
+                    showing.repeat_2()
+                if ch == '8':
+                    showing.MySQL_Screen_1()
+                    break
+
+        if ch == '5':
             struc.SQL_Shell()
-            showing.MySQL_Screen()
-            showing.repeat()
-        if ch == '8':
-            databaseName = raw_input('[*]Enter DataBase Name That You Want To Deal With It: ')
-            tableName = raw_input('[*]Enter Table Name That You Want To Deal With It: ')
-            deal.View_all(tableName, databaseName)
-            showing.repeat()
-        if ch == '9':
-            databaseName = raw_input('[*]Enter DataBase Name That You Want To Deal With It: ')
-            deal.Change_db(databaseName)
-            tableName = raw_input('[*]Enter Table Name That You Want To Deal With It: ')
-            deal.Add(tableName)
-            showing.repeat()
-        if ch == '10':
-            databaseName = raw_input('[*]Enter DataBase Name That You Want To Deal With It: ')
-            deal.Change_db(databaseName)
-            tableName = raw_input('[*]Enter Table Name That You Want To Deal With It: ')
-            deal.Update(tableName)
-            showing.repeat()
-        if ch == '11':
-            databaseName = raw_input('[*]Enter DataBase Name That You Want To Deal With It: ')
-            deal.Change_db(databaseName)
-            tableName = raw_input('[*]Enter Table Name That You Want To Deal With It: ')
-            deal.View(tableName, databaseName)
-            showing.repeat()
-        if ch == '12':
+            showing.MySQL_Screen_1()
+
+        if ch == '6':
             databaseName = raw_input('[*]Enter DataBase Name That You Want To Take Dump From It: ')
             deal.Dump(databaseName)
-            showing.repeat()
+            showing.repeat_1()
+
         if ch == '0':
             print '[**]bye'
             sys.exit()
